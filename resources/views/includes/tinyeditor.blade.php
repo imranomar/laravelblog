d3cdc5b413f91abb3c2021a59308c26e1486e5a6
Imran Omar Bukhsh <imranomar@gmail.com>
2019-07-01 15:01:33 +0400

    trying to add tiny editor

--- resources/views/includes/tinyeditor.blade.php
+++ resources/views/includes/tinyeditor.blade.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
 <script>
-    tinymce.init({selector:'textarea'});
+    tinymce.init({selector:'textarea', plugins: "image imagetools",
+        toolbar: "image",});
 
 
 </script>
